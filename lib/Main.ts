91acd4a3b4f48bf7e4959ac4508297a4f4dfc479
Callum Mackenzie <alexxander1611@gmail.com>
2021-05-27 22:40:48 -0600

    Demo simplifying.

--- lib/Main.ts
+++ lib/Main.ts
@@ -1,30 +1,34 @@
 "use strict";
 import * as IW from "./Ingenium.js";
 
-let shaders = {
-    three: null,
-    two: null
-};
+let shader3D: IW.Shader;
 let camera3D: IW.Camera3D = new IW.Camera3D();
-let am: IW.AnimatedMesh3D;
+let am: IW.AnimatedMesh3D[] = [];
 
 const onCreate = () => {
     IW.IngeniumWeb.defaultInit(0x000000);
-    shaders.three = IW.Shader.make3D();
-    shaders.two = IW.Shader.make2D();
-    console.log(IW.Shader.getAllShaderInfo());
-    let tmpMshs: IW.Mesh3D[] = [];
-    for (let i = 0; i < 2; i++)
-        tmpMshs.push(IW.Mesh3D.createAndMake("./resource/cubeanim/" + i.toString() + ".obj"));
-    am = new IW.AnimatedMesh3D(IW.Mesh3D.createEmpty(36), tmpMshs, 1, 500);
-    am.primaryMesh.position = new IW.Vec3(0, 0, 3);
+    shader3D = IW.Shader.make3D({
+        "lightModel": "PHONG",
+        "normalMap": "1"
+    });
+    let nCubes = 1;
+    for (let j = 0; j < nCubes; j++) {
+        let tmpMshs: IW.Mesh3D[] = [];
+        for (let i = 0; i < 2; i++)
+            tmpMshs.push(IW.Mesh3D.createAndMake("./resource/cubeanim/" + i.toString() + ".obj",
+                "./resource/paper/b.jpg", "./resource/paper/s.jpg", "./resource/paper/n.jpg"));
+        am.push(new IW.AnimatedMesh3D(IW.Mesh3D.createEmpty(36), tmpMshs, 1, 500));
+        am[j].primaryMesh.position = new IW.Vec3((j - (nCubes >> 1)) * 3, 0, 3);
+    }
 }
 
 const onUpdate = () => {
     camera3D.stdControl(4, IW.PI);
-    am.checkAdvanceFrame();
-    am.primaryMesh.rotation.addEquals(IW.Vec3.filledWith(0.4).mulFloat(IW.Time.deltaTime));
-    am.render(shaders.three, camera3D);
+    for (let i = 0; i < am.length; i++) {
+        am[i].checkAdvanceFrame();
+        am[i].primaryMesh.rotation.addEquals(IW.Vec3.filledWith(0.4).mulFloat(IW.Time.deltaTime));
+        am[i].render(shader3D, camera3D);
+    }
 }
 
 IW.IngeniumWeb.start([], onCreate, onUpdate);
--- out/Main.js
+++ out/Main.js
@@ -1,26 +1,29 @@
 "use strict";
 import * as IW from "./Ingenium.js";
-let shaders = {
-    three: null,
-    two: null
-};
+let shader3D;
 let camera3D = new IW.Camera3D();
-let am;
+let am = [];
 const onCreate = () => {
     IW.IngeniumWeb.defaultInit(0x000000);
-    shaders.three = IW.Shader.make3D();
-    shaders.two = IW.Shader.make2D();
-    console.log(IW.Shader.getAllShaderInfo());
-    let tmpMshs = [];
-    for (let i = 0; i < 2; i++)
-        tmpMshs.push(IW.Mesh3D.createAndMake("./resource/cubeanim/" + i.toString() + ".obj"));
-    am = new IW.AnimatedMesh3D(IW.Mesh3D.createEmpty(36), tmpMshs, 1, 500);
-    am.primaryMesh.position = new IW.Vec3(0, 0, 3);
+    shader3D = IW.Shader.make3D({
+        "lightModel": "PHONG",
+        "normalMap": "1"
+    });
+    let nCubes = 1;
+    for (let j = 0; j < nCubes; j++) {
+        let tmpMshs = [];
+        for (let i = 0; i < 2; i++)
+            tmpMshs.push(IW.Mesh3D.createAndMake("./resource/cubeanim/" + i.toString() + ".obj", "./resource/paper/b.jpg", "./resource/paper/s.jpg", "./resource/paper/n.jpg"));
+        am.push(new IW.AnimatedMesh3D(IW.Mesh3D.createEmpty(36), tmpMshs, 1, 500));
+        am[j].primaryMesh.position = new IW.Vec3((j - (nCubes >> 1)) * 3, 0, 3);
+    }
 };
 const onUpdate = () => {
     camera3D.stdControl(4, IW.PI);
-    am.checkAdvanceFrame();
-    am.primaryMesh.rotation.addEquals(IW.Vec3.filledWith(0.4).mulFloat(IW.Time.deltaTime));
-    am.render(shaders.three, camera3D);
+    for (let i = 0; i < am.length; i++) {
+        am[i].checkAdvanceFrame();
+        am[i].primaryMesh.rotation.addEquals(IW.Vec3.filledWith(0.4).mulFloat(IW.Time.deltaTime));
+        am[i].render(shader3D, camera3D);
+    }
 };
 IW.IngeniumWeb.start([], onCreate, onUpdate);
