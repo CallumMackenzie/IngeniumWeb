749d862f7ce498f5a9225ab4382cdab5f9448b6b
Callum Mackenzie <callum.alex.mackenzie@gmail.com>
2022-11-08 12:22:55 -0800

    Increase framerate

--- build/Main.js
+++ build/Main.js
@@ -3,8 +3,9 @@ import * as IW from "./Ingenium.js";
 let shader3D;
 let camera3D = new IW.Camera3D();
 let am = [];
+let m = [];
 const onCreate = () => {
-    IW.IngeniumWeb.defaultInit(0x000000);
+    IW.IngeniumWeb.defaultInit(0x000000, undefined, 60);
     shader3D = IW.Shader.make3D({
         "lightModel": "PHONG",
         "normalMap": "1"
@@ -17,14 +18,21 @@ const onCreate = () => {
         am.push(new IW.AnimatedMesh3D(IW.Mesh3D.createEmpty(36), tmpMshs, 1, 500));
         am[j].primaryMesh.position = new IW.Vec3((j - (nCubes >> 1)) * 3, 0, 3);
     }
+    let sphere = IW.Mesh3D.createAndMake("./resource/uvsmoothnt.obj", "./resource/metal/b.jpg", "./resource/metal/s.jpg", "./resource/paper/n.jpg");
+    sphere.position = new IW.Vec3(5, 0, 5);
+    m.push(sphere);
 };
 let ctr = 0;
 const onUpdate = () => {
     camera3D.stdControl(4, IW.PI);
-    for (let i = 0; i < am.length; i++) {
+    for (let i = 0; i < am.length; ++i) {
         am[i].checkAdvanceFrame();
         am[i].primaryMesh.rotation.addEquals(IW.Vec3.filledWith(0.4).mulFloat(IW.Time.deltaTime));
         am[i].render(shader3D, camera3D);
+    }
+    for (let i = 0; i < m.length; ++i) {
+        m[i].rotation.addEquals(IW.Vec3.filledWith(0.3).mulFloat(IW.Time.deltaTime));
+        m[i].render(shader3D, camera3D);
     }
 };
 IW.IngeniumWeb.start([], onCreate, onUpdate);
--- lib/Main.ts
+++ lib/Main.ts
@@ -4,9 +4,10 @@ import * as IW from "./Ingenium.js";
 let shader3D: IW.Shader;
 let camera3D: IW.Camera3D = new IW.Camera3D();
 let am: IW.AnimatedMesh3D[] = [];
+let m: IW.Mesh3D[] = [];
 
 const onCreate = () => {
-	IW.IngeniumWeb.defaultInit(0x000000);
+	IW.IngeniumWeb.defaultInit(0x000000, undefined, 60);
 	shader3D = IW.Shader.make3D({
 		"lightModel": "PHONG",
 		"normalMap": "1"
@@ -20,16 +21,25 @@ const onCreate = () => {
 		am.push(new IW.AnimatedMesh3D(IW.Mesh3D.createEmpty(36), tmpMshs, 1, 500));
 		am[j].primaryMesh.position = new IW.Vec3((j - (nCubes >> 1)) * 3, 0, 3);
 	}
+
+	let sphere = IW.Mesh3D.createAndMake("./resource/uvsmoothnt.obj",
+		"./resource/metal/b.jpg", "./resource/metal/s.jpg", "./resource/paper/n.jpg");
+	sphere.position = new IW.Vec3(5, 0, 5);
+	m.push(sphere);
 }
 
 let ctr = 0;
 
 const onUpdate = () => {
 	camera3D.stdControl(4, IW.PI);
-	for (let i = 0; i < am.length; i++) {
+	for (let i = 0; i < am.length; ++i) {
 		am[i].checkAdvanceFrame();
 		am[i].primaryMesh.rotation.addEquals(IW.Vec3.filledWith(0.4).mulFloat(IW.Time.deltaTime));
 		am[i].render(shader3D, camera3D);
+	}
+	for (let i = 0; i < m.length; ++i) {
+		m[i].rotation.addEquals(IW.Vec3.filledWith(0.3).mulFloat(IW.Time.deltaTime));
+		m[i].render(shader3D, camera3D);
 	}
 }
 
